ddee59354af722e2723f9d2605b711ac05edf9b2
Danuwarta13 <danujr650@gmail.com>
2025-06-07 13:40:47 +0700

    Class & Object

---
+++ Class-Object/coba.php
@@ -0,0 +1,13 @@
+<?php
+
+// Class Coba
+class Coba {}
+
+// instance coba / object dari kelas Coba
+$a = new Coba();
+$b = new Coba();
+$c = new Coba();
+
+var_dump($a);
+var_dump($b);
+var_dump($c);
---
+++ Class-Object/produk.php
@@ -0,0 +1,13 @@
+<?php
+
+// Jual Produk
+// Pulsa
+// Voucher
+
+// class produk
+class Produk {}
+
+// instance produk / object dari kelas Produk
+$produk1 = new Produk();
+$produk2 = new Produk();
+$produk3 = new Produk();
